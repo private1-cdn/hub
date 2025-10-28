a5dd7283b0c27f053b0b1e050dc811e2e1a66970
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2025-10-28 13:15:35 +0600

    tools.js

---
+++ data/tools.js
@@ -0,0 +1,14 @@
+const tools = [
+    {
+        id: 201,
+        name: "Downloader",
+        icon: "fas fa-download",
+        url: "https://example.com/downloader"
+    },
+    {
+        id: 202,
+        name: "Converter",
+        icon: "fas fa-exchange-alt",
+        url: "https://example.com/converter"
+    }
+];
