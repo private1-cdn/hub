fd77430073420b8a8fe723574ec0e85d163a7abb
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2025-10-28 13:11:26 +0600

    premiumUsers.js

---
+++ data/premiumUsers.js
@@ -0,0 +1,21 @@
+// Premium users data with names and expiry dates
+const premiumUsers = {
+    "01712345678": {
+        name: "VIP User 1",
+        expiryDate: "2024-12-12T22:10:00", // Expired date for testing
+        tvChannel: "yes",
+        openTools: "yes"
+    },
+    "01811223344": {
+        name: "Premium User 2", 
+        expiryDate: "2025-11-15T20:30:00",
+        tvChannel: "yes",
+        openTools: "yes"
+    },
+    "01900112233": {
+        name: "Gold User 3",
+        expiryDate: "2025-10-10T18:45:00",
+        tvChannel: "no",
+        openTools: "yes"
+    }
+};
