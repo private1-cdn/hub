6edc8bd4b4917e503ced01d5f31e582436b1e947
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2025-11-01 19:27:32 +0600

    tvchannels.js

--- data/tvchannels.js
+++ data/tvchannels.js
@@ -2,44 +2,9 @@
 
 const tvChannels = [
     {
-        name: "Music Channel",
-        thumbnail: "https://via.placeholder.com/640x360?text=Music+Channel",
-        m3u8Url: "https://media-hls.growcdnssedge.com/b-hls-12/196503633/196503633.m3u8"
-    },
-    {
-        name: "Sports TV",
-        thumbnail: "https://via.placeholder.com/640x360?text=Sports+TV",
-        m3u8Url: "https://webtvstream.bhtelecom.ba/pink_erotic1.m3u8"
-    },
-    {
-        name: "News Network",
-        thumbnail: "https://via.placeholder.com/640x360?text=News+Network",
-        m3u8Url: "https://media-hls.growcdnssedge.com/b-hls-03/184835341/184835341.m3u8"
-    },
-    {
-        name: "Entertainment Plus",
-        thumbnail: "https://via.placeholder.com/640x360?text=Entertainment+Plus",
-        m3u8Url: "https://media-hls.growcdnssedge.com/b-hls-29/212919195/212919195.m3u8"
-    },
-    {
-        name: "Movie Channel",
-        thumbnail: "https://via.placeholder.com/640x360?text=Movie+Channel",
-        m3u8Url: "https://media-hls.growcdnssedge.com/b-hls-29/212772444/212772444.m3u8"
-    },
-    {
-        name: "Documentary TV",
-        thumbnail: "https://via.placeholder.com/640x360?text=Documentary+TV",
-        m3u8Url: "https://media-hls.growcdnssedge.com/b-hls-12/196503633/196503633.m3u8"
-    },
-    {
-        name: "Kids Channel",
-        thumbnail: "https://via.placeholder.com/640x360?text=Kids+Channel",
-        m3u8Url: "https://webtvstream.bhtelecom.ba/pink_erotic1.m3u8"
-    },
-    {
-        name: "Lifestyle TV",
-        thumbnail: "https://via.placeholder.com/640x360?text=Lifestyle+TV",
-        m3u8Url: "https://media-hls.growcdnssedge.com/b-hls-03/184835341/184835341.m3u8"
+        name: "Comming Soon",
+        thumbnail: "#",
+        m3u8Url: "https://test.com/test.m3u8"
     }
 ];
 
