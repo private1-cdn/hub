5e99a6bd39b09a41c92575558ba367ffa2636a7b
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2025-11-01 19:23:35 +0600

    premiumusers.js

--- data/premiumusers.js
+++ data/premiumusers.js
@@ -2,16 +2,10 @@
 
 const premiumUsers = [
     {
-        phone: "01707665513",
-        name: "Premium User 1",
-        expiryDate: "31:12:2024",
-        expiryTime: "23:59"
-    },
-    {
-        phone: "01303665513",
-        name: "VIP Member",
-        expiryDate: "15:06:2024",
-        expiryTime: "12:00"
+        phone: "01606475942",
+        name: "Antor",
+        expiryDate: "02:12:2025",
+        expiryTime: "22:45"
     },
     {
         phone: "01645500055",
